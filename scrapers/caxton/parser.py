76e7162f6bcc980219492f0a2e0a814abbd9ea24
Adi Eyal <adi@burgercom.co.za>
2014-03-12 15:25:37 +0200

    Removed junk code

--- scrapers/caxton/parser.py
+++ scrapers/caxton/parser.py
@@ -63,39 +63,3 @@ def consume(job):
             import traceback
             traceback.print_exc()
 
-def parse():
-    g = Goose()
-    client = MongoClient()
-    db = client.article_db
-    articles = db.articles
-
-    for publication, feed_url in publications:
-        count = 0
-        feed = feedparser.parse(feed_url)
-        for entry in feed["entries"]:
-            try:
-                url = entry["link"]
-                if not articles.find_one({"url" : url}):
-                    count += 1
-                    article = g.extract(url=url)
-
-                    post = {
-                        "publication" : publication,
-                        "url" : url,
-                        "author" : entry["author"],
-                        "summary" : entry["summary"],
-                        "published" : entry["published"],
-                        "title" : article.title,
-                        "meta_description" : article.meta_description,
-                        "text" : article.cleaned_text
-                    }
-
-                    articles.insert(post)
-            except IOError, e:
-                print e.message
-                print url
-            except Exception, e:
-                import traceback
-                traceback.print_exc()
-
-        print "%s: %d articles added" % (publication, count)
