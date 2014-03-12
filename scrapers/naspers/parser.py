b6b7b047940870bafd54ed4eb24fbb750d60184a
Adi Eyal <adi@burgercom.co.za>
2014-03-12 20:19:54 +0200

    Parsing published date in naspers parser

--- scrapers/naspers/parser.py
+++ scrapers/naspers/parser.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import requests
 from urlparse import urlparse, urljoin
 from publications import publications
-#import beanstalkc
+from dateutil import parser as date_parser
 from ..config import beanstalk, articles
 
 g = Goose()
@@ -103,7 +103,7 @@ def consume(job):
     if not articles.find_one({"url" : url}):
         content = requests.get(url)
         post = parser.parse_html(content.text)
-        post["publication"] = job["publication"]
+        post["publication"] = date_parser.parse(job["publication"])
         post["url"] = url
         post["downloaded_at"] = datetime.datetime.now()
         articles.insert(post)
