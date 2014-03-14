fb2d1d5ddf7d55a65e2ea6d0b8b61dbd7e9f12ba
Adi Eyal <adi@burgercom.co.za>
2014-03-14 05:41:53 +0200

    Added News24 Feeds

--- scrapers/naspers_feeds/publications.py
+++ scrapers/naspers_feeds/publications.py
@@ -1,3 +1,12 @@
 publications = [
     ("City Press", "http://www.citypress.co.za/feed/"),
+    ("News24 - Top Stories", "http://feeds.24.com/articles/sport/featured/topstories/rss"),
+    ("News24 - South Africa", "http://feeds.news24.com/articles/channel/topstories/rss"),
+    ("News24 - Africa", "http://feeds.news24.com/articles/news24/Africa/rss"),
+    ("News24 - World", "http://feeds.news24.com/articles/news24/MyNews24/rss"),
+    ("News24 - Sci-Tech", "http://feeds.news24.com/articles/news24/opinions/rss"),
+    ("News24 - Sport", "http://feeds.news24.com/articles/news24/SciTech/rss"),
+    ("News24 - Entertainment", "http://feeds.news24.com/articles/news24/SouthAfrica/rss"),
+    ("News24 - MyNews24", "http://feeds.news24.com/articles/news24/TopStories/rss"),
+    ("News24 - Opinion", "http://feeds.news24.com/articles/news24/World/rss"),
 ]
