2c5e0d760076766ec24179989283ee5e4d50a3ff
Adi Eyal <adi@burgercom.co.za>
2014-03-16 23:13:28 +0200

    Added iq4news and a bunch of others

--- scrapers/independent_feeds/publications.py
+++ scrapers/independent_feeds/publications.py
@@ -7,4 +7,8 @@ publications = [
     ("Daily Dispatch - Sport", "http://www.dispatch.co.za/category/sport/feed/"),
     ("Matimu News", "http://matimunews.co.za/?feed=rss2"),
     ("Public Eye News", "http://www.publiceyenews.com/site/feed/"),
+    ("The Gremlin", "http://feeds.feedburner.com/co/JdcP?format=xml"),
+    ("The Newspaper", "http://www.thenewspaper.co.za/feed/"),
+    ("The SA News", "http://www.thesanews.co.za/feed/"),
+    ("IQ4 News", "http://www.iq4news.com/rss.xml"),
 ]
