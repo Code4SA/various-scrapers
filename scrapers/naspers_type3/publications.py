bfabaae908d4f879763a0800f0cf0075b26c0eb5
Adi Eyal <adi@burgercom.co.za>
2014-03-16 08:33:48 +0200

    Added Potchefstroom Herald

--- scrapers/naspers_type3/publications.py
+++ scrapers/naspers_type3/publications.py
@@ -1,4 +1,5 @@
 publications = [
     ("Kouga Express - News", "http://www.kougaexpress.co.za/News/articles"),
     ("Kouga Express - Sport", "http://www.kougaexpress.co.za/Sport/articles"),
+    ("Potchefstroom Herald", "http://www.potchefstroomherald.co.za/News/articles"),
 ]
