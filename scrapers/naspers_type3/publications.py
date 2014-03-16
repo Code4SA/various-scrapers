644ec6323255070fff7fe8946f0c164cd8ed1299
Adi Eyal <adi@burgercom.co.za>
2014-03-16 08:39:35 +0200

    Added Uitenhage Despatch

--- scrapers/naspers_type3/publications.py
+++ scrapers/naspers_type3/publications.py
@@ -1,5 +1,8 @@
 publications = [
     ("Kouga Express - News", "http://www.kougaexpress.co.za/News/articles"),
     ("Kouga Express - Sport", "http://www.kougaexpress.co.za/Sport/articles"),
-    ("Potchefstroom Herald", "http://www.potchefstroomherald.co.za/News/articles"),
+    ("Potchefstroom Herald - News", "http://www.potchefstroomherald.co.za/News/articles"),
+    ("Potchefstroom Herald - Sport", "http://www.potchefstroomherald.co.za/Sport/articles"),
+    ("Uitenhage Despatch - News", "http://www.ud-news.co.za/News/articles"),
+    ("Uitenhage Despatch - Sport", "http://www.ud-news.co.za/Sport/articles"),
 ]
