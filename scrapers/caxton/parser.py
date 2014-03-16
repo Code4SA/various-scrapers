45573a8cb67e803e2a2cef126757bc989e0aee77
Adi Eyal <adi@burgercom.co.za>
2014-03-16 22:19:48 +0200

    Moved caxton to a BasicFeedScraper format

--- scrapers/caxton/parser.py
+++ scrapers/caxton/parser.py
@@ -1,65 +1,11 @@
-import json
-import logging
-import datetime
-import feedparser
-import os
-import requests
-from pymongo import MongoClient
-from goose import Goose
-from caxton import publications
-from .. import config
-from ..config import articles, beanstalk
-from dateutil import parser as date_parser
+from publications import publications
+from ..scrapers import BasicFeedScraper
 
-logger = logging.getLogger(__name__)
-g = Goose()
+scraper = BasicFeedScraper(publications, "caxton_local", "Caxton", sub_type=2)
 
 def produce():
-    for publication, feed_url in publications:
-        feed = feedparser.parse(feed_url)
-        for entry in feed["entries"]:
-            try:
-                url = entry["link"]
-                print url
-                beanstalk.put(json.dumps({
-                    "url" : url,
-                    "scraper" : "caxton_local",
-                    "publication" : publication,
-                    "entry" : {
-                        "author" : entry["author"],
-                        "summary" : entry["summary"],
-                        "published" : entry["published"],
-                    }
-                }))
-            except Exception, e:
-                logger.error("Error putting a message into the queue", exc_info=True)
-
+    scraper.produce()
 
 def consume(job):
-    url = job["url"]
-    entry = job["entry"]
-    print url
-    if not articles.find_one({"url" : url}):
-        try:
-            article = g.extract(url=url)
-
-            post = {
-                "publication" : job["publication"],
-                "url" : url,
-                "author" : entry["author"],
-                "summary" : entry["summary"],
-                "published" : date_parser.parse(entry["published"]),
-                "title" : article.title,
-                "meta_description" : article.meta_description,
-                "text" : article.cleaned_text,
-                "owner" : "Caxton",
-                "sub_type" : 2,
-                "downloaded_at" : datetime.datetime.now()
-            }
-
-            articles.insert(post)
-        except IOError, e:
-            logger.error("IOError running Goose", exc_info=True)
-        except Exception, e:
-            logger.error("General article scraping exception", exc_info=True)
+    scraper.consume(job)
 
