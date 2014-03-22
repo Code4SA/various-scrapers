a7591d99a4ac0a5e322c92c24491eb8ee67e60e6
Adi Eyal <adi@burgercom.co.za>
2014-03-22 23:10:22 +0200

    Added url2soup function

---
+++ scrapers/utils.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+import logging
+
+logger = logging.getLogger(__name__)
+
+def url2soup(url):
+    try:
+        r = requests.get(url)
+        content = r.content.decode("utf8")
+        soup = BeautifulSoup(content, "html5lib")
+        return soup
+    except UnicodeEncodeError:
+        logger.exception("Could not decode page")
+    except Exception:
+        logger.exception("An error occurred while downloading html")
+
