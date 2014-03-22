2b261ec901e327e2d1d13e288f66291597e233df
Adi Eyal <adi@burgercom.co.za>
2014-03-22 23:41:22 +0200

    Added additional sections to financial mail

--- scrapers/financialmail/publications.py
+++ scrapers/financialmail/publications.py
@@ -1,3 +1,8 @@
 publications = [
     ("Financial Mail - FMFox", "http://www.financialmail.co.za/fmfox/"), 
+    ("Financial Mail - Features", "http://www.financialmail.co.za/features/"), 
+    ("Financial Mail - Money and Investing", "http://www.financialmail.co.za/moneyinvesting/"), 
+    ("Financial Mail - Media and Advertising", "http://www.financialmail.co.za/mediaadvertising/"), 
+    ("Financial Mail - Opinion", "http://www.financialmail.co.za/opinion/"), 
+    ("Financial Mail - Life", "http://www.financialmail.co.za/life/"), 
 ]
