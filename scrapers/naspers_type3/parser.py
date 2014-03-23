8aa5a0c672c920adf5015a02ed1b9450db9701eb
Adi Eyal <adi@burgercom.co.za>
2014-03-23 09:39:04 +0200

    Manual scraping of Kouga express

--- scrapers/naspers_type3/parser.py
+++ scrapers/naspers_type3/parser.py
@@ -45,21 +45,48 @@ class Scraper(object):
         entry = job["entry"]
         if not articles.find_one({"url" : url}):
             try:
+                soup = url2soup(url)
+                if soup:
+                    info = soup.select(".article_info")
+                    if not info: return None
+                    info = info[0]
+                    divs = info.select("div")
+                    if not len(divs) > 1: return None
+
+                    info2 = info.select("div")[1]
+                    divs = info2.select("div")
+                    if not len(divs) > 2: return None
+
+                    info3 = divs[4]
+                    divs = info3.select("div")
+                    title = divs[0].text
+                    author = divs[1].text
+                    try:
+                        publish_date = date_parser.parse(divs[3].text)
+                    except TypeError:
+                        logging.exception("Error parsing published date: %s" % url)
+                    summary = divs[4].text
+
+                    body = ""
+                    ps = soup.select(".article_body_font p")
+                    for p in ps:
+                        body += p.text + "\n\n"
+                     
                 article = g.extract(url=url)
                 return {
                     "publication" : job["publication"],
                     "url" : job["url"],
-                    "author" : article.author if hasattr(article, "author") else "",
-                    "summary" : entry["summary"],
+                    "author" : author, 
+                    "summary" : entry.get("summary", "") or summary,
                     "published" : date_parser.parse(entry["published"]),
                     "title" : entry["title"],
-                    "text" : article.cleaned_text,
+                    "text" : body,
                     "owner" : "Naspers/Media24",
                     "sub_type" : 3,
                     "downloaded_at" : datetime.datetime.now()
                 }
             except UnicodeEncodeError:
-                logging.exception("Error parsing url - possibly unicode url")
+                logging.exception("Error parsing url - possibly unicode url: %s" % url)
  
 scraper = Scraper(publications)
 def produce():
