1cab6402da6f293962a4f0e31ef6ed591cb984a0
Adi Eyal <adi@burgercom.co.za>
2014-04-06 21:27:58 +0200

    Changed logging to debug from info

--- scraper.py
+++ scraper.py
@@ -13,10 +13,10 @@ logger = logging.getLogger(__name__)
 def consumer():
     logger.info("Starting consumer")
     while True:
-        logger.info("Waiting for job")
+        logger.debug("Waiting for job")
         job = beanstalk.reserve()
         scrape_job = json.loads(job.body)
-        logger.info("Job payload: %s" % scrape_job)
+        logger.debug("Job payload: %s" % scrape_job)
         scraper_name = scrape_job["scraper"]
         scraper = scrapermap[scraper_name]
         post = scraper.consume(scrape_job)
